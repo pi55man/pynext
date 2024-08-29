0017298a5868dfe2f564bef84cce6ef8675ef321
pi55man <cuinnv@gmail.com>
2024-08-30 00:39:48 +0530

    passing values back and forth

--- server/server.py
+++ server/server.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-
 
 
 app = Flask(__name__)
@@ -9,9 +8,21 @@ CORS(app)
 @app.route('/api/home',methods = ['GET'])
 def return_home():
     return jsonify({
-        'message': 'hello world'
+        'message': 'hello lolz'
+    })
+
+@app.route('/api/get',methods=["POST"])
+def take_val():
+    data = request.json
+    value = data.get('value')
+    print(value)
+    return jsonify({
+        'reciveed' : value
     })
 
 
+   
+
 if __name__ == '__main__':
     app.run(debug = True,port=8080)
+    
